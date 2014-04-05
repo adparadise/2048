dd5b59fef26231b79230937561efc56dcedc0726
Andrew Paradise <adparadise@gmail.com>
2014-04-05 10:22:53 -0400

    add some zen

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,20 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+    var messages = {
+        2: "sensation",
+        4: "experience",
+        8: "thought",
+        16: "suffering",
+        32: "tranquility",
+        64: "mindfulness",
+        128: "wisdom",
+        256: "rapture",
+        512: "enlightenment",
+        1024: "selflessness",
+        2048: "nirvana"
+    };
+  inner.textContent = messages[tile.value];
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
@@ -126,7 +139,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You've achieved Nirvana!" : "You are mired in the flesh!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
